36cfdf1dd38248522670995d85aa0e18e837c1de
maxwellbenton <maxwellbenton@gmail.com>
2019-03-13 19:14:25 -0400

    recreating solution

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here
+const var1 = 2
+const var2 = 31
+const multiply = var1 * var2
+const random = Math.floor( Math.random() * 1000 ) + 1
+const mod = 16 % 6
+const max = Math.max( 5, 10, 15, 20 )
