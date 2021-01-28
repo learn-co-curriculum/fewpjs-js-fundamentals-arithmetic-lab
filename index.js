22c05aa22b507a2fe06d00eec2c6a5fd86096d5b
Liz Burton <liz.burton147@gmail.com>
2021-01-28 14:08:22 -0500

    Update solution code

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const var1 = 2
-const var2 = 31
-const multiply = var1 * var2
-const random = Math.floor( Math.random() * 1000 ) + 1
-const mod = 16 % 6
-const max = Math.max( 5, 10, 15, 20 )
+// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.floor( Math.random() * 1000 ) + 1;
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+const max = Math.max( 5, 10, 15, 20 );
